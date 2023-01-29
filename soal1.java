9237817d31307ed81d4817e1fd182e9cdb6d2582
bobmnueel <123434936+bobmnueel@users.noreply.github.com>
2023-01-29 12:24:02 +0700

    Update soal1.java

--- soal1.java
+++ soal1.java
@@ -1,7 +1,28 @@
-/*      Deskripsi  
-//       Soal 1     
-   Nama     : Bob Manuel
-   NIM      : 221524038
-   Kelas    : D4-1B
+/*      Deskripsi      
+ * Nama     : Bob Manuel
+ * NIM      : 221524038
+ * Kelas    : D4-1B
+ *
+ * Soal 1   : Input&Ouput
 */
 
+import java.util.Scanner;
+public class Soal1 {
+    public static void main(String args[]) {
+        
+        String kata;
+        int jumlahkata;
+        
+        Scanner sc = new Scanner(System.in);
+        kata = sc.nextLine();
+        sc.close();
+        
+        String[] kataArray = kata.split("[ !,?._'@]+");
+        jumlahkata = kataArray.length;
+        
+        System.out.println(jumlahkata);
+        for(int i = 0; i < jumlahkata; i++){
+            System.out.println(kataArray[i]);
+        }
+    }
+}
